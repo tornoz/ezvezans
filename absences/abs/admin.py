7b4746b96621bf6caec5745d9a60aa5b6f31f0d8
gmr <gwenn@archbang>
2014-02-13 23:08:29 +0100

    modfied for model change

--- absences/abs/admin.py
+++ absences/abs/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from abs.models import Absence
+from abs.models import Etudiant
+from abs.models import Enseignant
+from abs.models import Cours
+from abs.models import Justificatif
+from abs.models import Secretaire
 
-# Register your models here.
+
+class UtilisateurAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('identifiant', 'user')
+    
+
+admin.site.register(Absence)
+admin.site.register(Etudiant, UtilisateurAdmin)
+admin.site.register(Enseignant, UtilisateurAdmin)
+admin.site.register(Cours)
+admin.site.register(Justificatif)
+admin.site.register(Secretaire, UtilisateurAdmin)
--- absences/abs/models.py
+++ absences/abs/models.py
@@ -1,11 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Utilisateur(models.Model):
     identifiant = models.CharField(max_length=200)
-    nom = models.CharField(max_length=200)
-    prenom = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    user = models.ForeignKey(User);
     class Meta:
         abstract = True
 
@@ -15,8 +14,20 @@ class Etudiant(Utilisateur):
 
 class Enseignant(Utilisateur):
     departement = models.CharField(max_length=200)
+    
+class Secretaire(Utilisateur):
+	departement = models.CharField(max_length=200)
 
 class Cours(models.Model):
     dateDebut = models.DateTimeField()
     dateFin = models.DateTimeField()
     enseignant = models.ForeignKey(Enseignant)
+
+class Absence(models.Model):
+    cours = models.ForeignKey(Cours)
+    etudiant = models.ForeignKey(Etudiant)
+
+class Justificatif(models.Model):
+    dateDebut = models.DateTimeField()
+    dateFin = models.DateTimeField()
+    etudiant = models.ForeignKey(Etudiant)
