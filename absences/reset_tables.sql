518d8c802298f0d0685552f15df931fcad345661
gmr <gwenn@archbang>
2014-02-16 18:55:43 +0100

    updated some things on model

---
+++ absences/reset_tables.sql
@@ -0,0 +1,8 @@
+drop table abs_etudiant;
+drop table abs_enseignant;
+drop table abs_cours;
+drop table abs_justificatif;
+drop table abs_secretaire;
+drop table abs_promotion;
+drop table abs_absence;
+
