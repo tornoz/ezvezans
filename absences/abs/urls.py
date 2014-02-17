fb3d052e99c1a1428aae76a6bfd38017df169827
Amaury@LOCALHOST <Amaury@LOCALHOST>
2014-02-17 12:53:54 +0100

    Ajout d'un champ file pour l'upload de fichier. Nouveau formulaire spécial pour les Justificatif. Vue secretaire commencée, liste des absences dont valide=false

--- absences/abs/urls.py
+++ absences/abs/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     url(r'^login/', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page':'/abs/login'}),
     url(r'^add/(?P<entity>\w+)', views.add, name='add'),
+    url(r'^add_justificatif/', views.add_justificatif, name='add_justificatif'),
     url(r'^$', views.index, name='index')
 )
